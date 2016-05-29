2ae69331e59ba72387a30e8730ca930d760a4f62
JeremySMorgan <jsmorgan6@gmail.com>
2016-05-29 01:40:39 +0000

    functioning servo control

--- NodeServer/main.py
+++ NodeServer/main.py
@@ -1,6 +1,7 @@
 import smbus
 import time
 import random
+import sys
 
 # for RPI version 1, use bus = smbus.SMBus(0)
 bus = smbus.SMBus(1)
@@ -12,12 +13,13 @@ def writeData(arrayData):
 	for i in range(len(arrayData)):
 		bus.write_byte(address,arrayData[i]);
 
-ledVal = sys.argv[0]
-verticalVal = sys.argv[1]
-horizantalVal = sys.argv[2]
+
+ledVal = int(sys.argv[1])
+verticalVal = int(sys.argv[2])
+horizantalVal = int(sys.argv[3])
 
 array = [ledVal,verticalVal,horizantalVal];
-
+print array
 writeData(array)
 
 print "data sent"
