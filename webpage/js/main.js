800e4aa216aec6c71d0e2dd0a13fdfc156346abc
admin <jsmorgan6@gmail.com>
2016-11-11 20:16:43 -0500

    remove unneeded code

--- webpage/js/main.js
+++
@@ -1,9 +0,0 @@
-console.log("js working")
-/*$.ajax({
-        url: 'http://freegeoip.net/json/',
-        type: 'POST',
-        dataType: 'jsonp',
-        success: function(location) {
-            alert(location.ip);
-        }
-    });*/
