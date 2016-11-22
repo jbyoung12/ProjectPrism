93ff33d05135ce957ec91f837f128af2fe89d780
admin <jsmorgan6@gmail.com>
2016-11-22 16:48:58 -0500

    360 Scanner Calculations
    Current design not possible due to memory constraints on Arduino - need
    to implement more efficient design.

--- _360_Scanner/_360_Scanner.ino
+++ _360_Scanner/_360_Scanner.ino
@@ -1,11 +1,13 @@
+/*
+ * Author:  Jeremy Morgan
+ * Date:    11/22/16
+ * Notes:   Current design will not work due to insufficient memory on board. Solution is to do calculations in real time in loop, rather than store data and then analyze. 
+ *          If real time analysis is not possible, transition sonar system to raspberry pi.
+ */
+
 #include <NewPing.h>
-#include <Servo.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-#include <SPI.h>
-#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
-#define SLAVE_ADDRESS 0x05
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
@@ -14,7 +16,9 @@ const int ScannerServoFrontRight =  3;
 const int ScannerServoBackLeft =    11; 
 const int ScannerServoBackRight =   15;
 
-int distances[360]; 
+#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+#define SLAVE_ADDRESS 0x05
 
 #define TRIGGER_PIN_FRONT_RIGHT  2
 #define ECHO_PIN_FRONT_RIGHT    3
@@ -30,77 +34,121 @@ int distances[360];
 
 #define MAX_DISTANCE 200
 
+
 NewPing FrontRight( TRIGGER_PIN_FRONT_RIGHT,  ECHO_PIN_FRONT_RIGHT, MAX_DISTANCE);
 NewPing FrontLeft(  TRIGGER_PIN_FRONT_LEFT,   ECHO_PIN_FRONT_LEFT,  MAX_DISTANCE);
 NewPing BackRight(  TRIGGER_PIN_BACK_RIGHT,   ECHO_PIN_BACK_RIGHT,  MAX_DISTANCE);
 NewPing BackLeft(   TRIGGER_PIN_BACK_LEFT,    ECHO_PIN_BACK_LEFT,   MAX_DISTANCE);
 
+
+int distances[360]; 
+int frontLeftDistances[180];
+int frontRightDistances[180];
+int backLeftDistances[180];
+int backRightDistances[180];
+
+const double distanceFromCenterToServo = 16.5;
+
+double get_B_Length(double a, double c, double bAngleDeg);
+
 void setup() {
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
   Serial.begin(9600);
+  
+  resetValues();
 }
-
-
-// ScannerServoFrontRight is fucked up
 
 void loop() {
 
-      for (int i = 0; i < 180; i ++ ){
-        angle(ScannerServoFrontLeft,i);
-        angle(ScannerServoFrontRight,i);
-        angle(ScannerServoBackLeft,i);
-         angle(ScannerServoBackRight,i);
-        delay(15);
-    }
-
-      for (int i = 180; i > 0; i -- ){
-        angle(ScannerServoFrontLeft,i);
-        angle(ScannerServoFrontRight,i);
-        angle(ScannerServoBackLeft,i);
-         angle(ScannerServoBackRight,i);
-        delay(15);
-    }
-
-  /*
-  
-    for (int i = 0; i < 180; i ++ ){
-        angle(ScannerServoFrontLeft,i);
-        Serial.println(FrontLeft.ping_cm());
-        delay(15);
-    }
-    angle(ScannerServoFrontLeft,90);
-
-    delay(2000);
-
-    for (int i = 0; i < 180; i ++ ){
-        angle(ScannerServoFrontRight,i);
-        Serial.println(FrontRight.ping_cm());
-        delay(15);
-    }
+  for (int i = 0; i < 180; i++){
+    angle(ScannerServoFrontLeft,  i);
+    angle(ScannerServoFrontRight, i);
+    angle(ScannerServoBackLeft,   i);
+    angle(ScannerServoBackRight,  i);
     
-    angle(ScannerServoFrontRight,90);
-    delay(2000);
+    frontLeftDistances[i]   =  FrontLeft.ping_cm();
+    frontRightDistances[i]  =  FrontRight.ping_cm();
+    backLeftDistances[i]    =  BackLeft.ping_cm();
+    backRightDistances[i]   =  BackRight.ping_cm();
+  }
+
+  // FRONT RIGHT CENTER VALUES
+  for (int i = 0; i < 45; i ++){
+    
+      int FrontRightSonarReading = frontRightDistances[i];
+      double SonarReadingDistanceFromCenter = get_B_Length( FrontRightSonarReading, distanceFromCenterToServo, 90+i);
+      double AngleFromCenter = getA_Angle(FrontRightSonarReading, SonarReadingDistanceFromCenter, distanceFromCenterToServo);
+      int translatedAngle = getValidAngle( (int) (45 - AngleFromCenter));
+
+      // The angle is overlapping with quadrant 4
+      if (translatedAngle < 0){
+        // the angle has not been set before
+        if (distances[translatedAngle] == -1){
+          distances[i] = SonarReadingDistanceFromCenter;
+        }
+        else{
+            distances[translatedAngle] = (int) ((distances[translatedAngle] + SonarReadingDistanceFromCenter)/2);
+        }
+      }
+      else {
+        distances[i] = SonarReadingDistanceFromCenter;
+      }
+  }
+
+  for (int i = 45; i < 135; i ++){
+      int FrontRightSonarReading = frontRightDistances[i];
+      double SonarReadingDistanceFromCenter = get_B_Length( FrontRightSonarReading, distanceFromCenterToServo, 90+i);
+      double AngleFromCenter = getA_Angle(FrontRightSonarReading, SonarReadingDistanceFromCenter, distanceFromCenterToServo);
+      int translatedAngle = getValidAngle( (int) (45 - AngleFromCenter));
+      
+  }
 
 
-    for (int i = 0; i < 180; i ++ ){
-      angle(ScannerServoBackLeft,i);
-      Serial.println(BackLeft.ping_cm());
-      delay(15);
-    }
-    angle(ScannerServoBackLeft,90);
-    delay(2000);
 
-    for (int i = 0; i < 180; i ++ ){
-      angle(ScannerServoBackRight,i);
-      Serial.println(BackRight.ping_cm());
-      delay(15);
-    }
-    angle(ScannerServoBackRight,90);
-    delay(2000);
-*/
+  for (int i = 0; i < 360; i++){
+    Serial.print("Angle "+String(i)+": "+String(distances[i]));  
+  }
+  Serial.println();
+  delay(1000);
 }
 
+int getValidAngle(int angle){
+
+  if (angle < 0){
+    while (angle<0){
+      angle = angle + 360;
+    }  
+    return angle;
+  }
+  else {
+    return angle % 360;  
+  }
+}
+
+double getA_Angle(int a, int b, int c){
+
+  double top = pow(a,2);
+  top = top - pow(b,2) ;
+  top = top - pow(c,2);
+  double bottom = (-2 * b * c);
+  double frac = top/bottom;
+  double ret = acos(frac) ;
+  ret = ret * (57296/1000);
+  return ret;
+}
+
+double get_B_Length(double a, double c, double bAngleDeg){
+   double bAngleRad = bAngleDeg * 1000 / 57296;
+   double squareRooted = pow(a,2) + pow (c,2) ;
+   squareRooted = squareRooted - ( 2 * a * c * cos(bAngleRad)   );
+   double retVal = sqrt(squareRooted);
+   return retVal;
+}
+
+int midVal(int x1, int x2){
+  return  (int) ( x1 + (x2-x1)/2 ); 
+}
 
 int limiter(int input) {
 
@@ -118,7 +166,7 @@ int limiter(int input) {
 
 //leg number, angle
 void angle(int OutputLine, int Angle) {
-  pwm.setPWM(OutputLine, 0, returnMappedValue(Angle)); // center front right MIDDLE
+  pwm.setPWM(OutputLine, 0, limiter(returnMappedValue(Angle))); // center front right MIDDLE
 }
 
 int returnMappedValue(int value) {
@@ -126,6 +174,17 @@ int returnMappedValue(int value) {
   return value;
 }
 
+void resetValues(){
+   for (int i = 0 ; i < 360; i ++){
+    distances[i] = -1;
+  }
+  for (int i = 0 ; i < 180; i ++){
+    frontLeftDistances[i]   = -1;
+    frontRightDistances[i]  = -1;
+    backLeftDistances[i]    = -1;
+    backRightDistances[i]   = -1;
+  }
 
+}
 
 
